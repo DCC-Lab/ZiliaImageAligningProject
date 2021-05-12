2eef2e32d4b34840098b5f370af14742889fd91d
Emile Marcotte <emilemarcotte@outlook.com>
2021-05-12 16:22:34 -0400

    Updated test file to better visualize pictures

--- Elahe/tests/format_test.py
+++ Elahe/tests/format_test.py
@@ -7,15 +7,34 @@ sys.path.insert(1, directory)
 from processImages import *
 from analyseRosaImages import *
 
-M = np.array([[[0,0,1], [0,1,1], [1,0,1]], [[1,1,1], [1,1,1], [1,1,1]], [[1,1,1], [1,1,1], [1,1,1]]])
+# M = np.array([[[0,0,1], [0,1,1], [1,0,1]], [[1,1,1], [1,1,1], [1,1,1]], [[1,1,1], [1,1,1], [1,1,1]]])
+# print(M[0,:,:])
 
 np.random.seed(1)
-randomPicture = (np.random.rand(3,3,3)*255).astype(np.uint8)
+randomPicture1 = (np.random.rand(3,3,3)*255).astype(np.uint8)
+np.random.seed(2)
+randomPicture2 = (np.random.rand(3,3,3)*255).astype(np.uint8)
+np.random.seed(3)
+randomPicture3 = (np.random.rand(3,3,3)*255).astype(np.uint8)
 
-plt.imshow(randomPicture)
-plt.imsave("random3x3pic.png", randomPicture)
+plt.imshow(randomPicture1)
+plt.imsave("random3x3pic1.jpg", randomPicture1)
+plt.show()
+plt.imshow(randomPicture2)
+plt.imsave("random3x3pic2.jpg", randomPicture2)
+plt.show()
+plt.imshow(randomPicture3)
+plt.imsave("random3x3pic3.jpg", randomPicture3)
 plt.show()
 
-print(randomPicture)
+# print(randomPicture)
 
-# print(M[0,:,:])
+directory = getCollectionDirectory()
+grayImage = loadImages(directory)
+
+plt.imshow(grayImage)
+plt.show()
+plt.imsave("grayImage.jpg", grayImage)
+
+print(grayImage)
+
