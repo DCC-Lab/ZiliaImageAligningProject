a62f05142c2db6e910d8fa5474e7eaa54243348f
Daniel <dccote@cervo.ulaval.ca>
2021-05-14 10:35:06 -0400

    First tests

--- Elahe/tests/flipMatrixTest.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-A = np.diag([1, 2, 3, 4, 5])
-print(A)
-
-A = np.fliplr(A)
-
-
-print(A)
-
---
+++ Elahe/tests/testsFlipMatrix.py
@@ -0,0 +1,37 @@
+import envtest
+import numpy as np
+
+
+class TestFlipMatrix(envtest.ZiliaTestCase):
+    def testNumpyAll(self):
+        A = np.array([1,2])
+
+    def testNumpyDiagonalMatrix(self):
+        A = np.diag([1, 2, 3, 4, 5])
+        self.assertIsNotNone(A)
+        for i in range(5):
+            for j in range(5):
+                if i == j:
+                    self.assertTrue(A[i,j] != 0)
+                else:
+                    self.assertTrue(A[i,j] == 0)
+
+    def testNumpyFlipLRDiagonal(self):
+        A = np.diag([1, 2, 3, 4, 5])
+        self.assertIsNotNone(A)
+        B = np.fliplr(A)
+        self.assertIsNotNone(B)
+        # self.assertTrue(A.all() == B.all())
+
+    def testNumpyFlipLRArbitrary(self):
+        A = np.array([[1, 2],[3,4]])
+        self.assertIsNotNone(A)
+        B = np.fliplr(A)
+        self.assertIsNotNone(B)
+        self.assertEqual(B[0,0], 2)
+        self.assertEqual(B[0,1], 1)
+        self.assertEqual(B[1,0], 4)
+        self.assertEqual(B[1,1], 3)
+  
+if __name__ == '__main__':
+    envtest.main()
