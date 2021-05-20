a59ed50dcff4d866c082fc83b401e9f7be04b81a
Emile Marcotte <emilemarcotte@outlook.com>
2021-05-20 10:11:32 -0400

    Init of test file for the loadNewImages function

---
+++ tests/testLoadNewImages.py
@@ -0,0 +1,12 @@
+import envtest
+from processImages import *
+
+class TestLoadNewImages(envtest.ZiliaTestCase):
+
+    def testInit(self):
+        self.assertTrue(True)
+
+
+
+if __name__ == "__main__":
+    envtest.main()
