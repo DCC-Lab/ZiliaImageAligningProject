630bee1732da0b5593a49aa638b60adbf9b35ecc
Emile Marcotte <emilemarcotte@outlook.com>
2021-05-20 10:39:47 -0400

    Tests for loadNewImages vs loadImages

--- tests/testLoadNewImages.py
+++ tests/testLoadNewImages.py
@@ -1,5 +1,6 @@
 import envtest
 from processImages import *
+import numpy as np
 
 oldFilesTestDir = r"C:\Users\elm77\OneDrive\Documents\ULaval\2021_2_Ete\CERVO\Projet\code\mini_test_sample"
 newFilesTestDir = r"C:\Users\elm77\OneDrive\Documents\ULaval\2021_2_Ete\CERVO\Projet\code\mini_test_sample_newdata"
@@ -11,14 +12,24 @@ class TestLoadNewImages(envtest.ZiliaTestCase):
         self.assertTrue(True)
 
     def testImportNewFiles(self):
-        pass
+        newImages = loadNewImages(newFilesTestDir)
+        self.assertIsNotNone(newImages)
+        self.assertTrue(len(newImages) > 0)
+        self.assertTrue(len(newImages) < 100)
 
     def testImportOldFiles(self):
-        pass
+        oldImages = loadNewImages(oldFilesTestDir)
+        self.assertIsNotNone(oldImages)
+        self.assertTrue(len(oldImages) > 0)
+        self.assertTrue(len(oldImages) < 100)
 
     def testImportOldFilesDoesTheSameAsTheOldFunction(self):
-        pass
-
+        imagesOldFunction = loadImages(oldFilesTestDir)
+        self.assertIsNotNone(imagesOldFunction)
+        imagesNewFunction = loadNewImages(oldFilesTestDir)
+        self.assertIsNotNone(imagesNewFunction)
+        equalityTest = np.equal(imagesOldFunction, imagesNewFunction)
+        self.assertTrue(equalityTest.all())
 
 
 if __name__ == "__main__":
