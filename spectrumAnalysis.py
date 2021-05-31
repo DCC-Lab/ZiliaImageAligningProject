3cc40349fec3bc871295b283728f5d1228929bed
ElaheParham <59700098+ElaheParham@users.noreply.github.com>
2021-05-31 12:00:16 -0400

    update spectrum analysis

--- spectrumAnalysis.py
+++ spectrumAnalysis.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
 
 
 def loadComponentesSpectra():
+    '''load components spectrums for the analysis'''
     spectrumComponents = pd.read_csv (r'_components_spectra.csv')
     npComponents=spectrumComponents.to_numpy()
     wavelengths=npComponents[:,0]
@@ -27,12 +30,53 @@ def loadComponentesSpectra():
     return components_spectra
 
 def loadWhiteRef():
+    ''' returns cropped (between 500 to 600) white reference and the wavelength'''
     RefNothingInfront = pd.read_csv ('int75_LEDON_nothingInFront.csv',sep=',',skiprows=23).to_numpy()
     RefWhite = pd.read_csv ('int75_WHITEREFERENCE.csv',sep=',',skiprows=23).to_numpy()
     wavelengthRef=RefWhite[:,1]
-    print(np.mean(RefWhite[4:],axis=1)-np.mean(RefNothingInfront[4:],axis=1))
-    return np.mean(RefWhite[4:],axis=1)-np.mean(RefNothingInfront[4:],axis=1),wavelengthRef
+    wRef=np.mean(RefWhite[:,4:],axis=1)-np.mean(RefNothingInfront[:,4:],axis=1)
+    wavelengthCropped=wavelengthRef[np.where(np.logical_and(500<= wavelengthRef, wavelengthRef <= 600))]
+    RefCropped = wRef[np.where(np.logical_and(500 <= wavelengthRef, wavelengthRef <= 600))]
+    return RefCropped,wavelengthCropped
+
+def loadDarkRef():
+    ''' returns cropped (between 500 to 600) dark reference and the wavelength'''
+    filetypes = [("csv files", "*.csv")]
+    csv_file_path = askopenfilename(title="select the dark reference .csv file",filetypes=filetypes)
+    darkRef = pd.read_csv(csv_file_path, sep=',', skiprows=4).to_numpy()
+    dRef = np.mean(darkRef[:,3:],axis=1)
+    wavelengthDark = darkRef[:,0]
+    wavelengthCropped=wavelengthDark[np.where(np.logical_and(500<= wavelengthDark, wavelengthDark <= 600))]
+    darkRefCropped = dRef[np.where(np.logical_and(500<= wavelengthDark, wavelengthDark <= 600))]
+    return darkRefCropped,wavelengthCropped
+
+def loadSpectrum():
+    ''' returns dark reference and the wavelength'''
+    filetypes = [("csv files", "*.csv")]
+    csv_file_path = askopenfilename(title="select the spectrum .csv file", filetypes=filetypes)
+    spectrumData = pd.read_csv(csv_file_path, sep=',', skiprows=4).to_numpy()
+    spectrum=spectrumData[:, 3:]
+    specWavelength=spectrumData[:, 0]
+    wavelengthCropped = specWavelength[np.where(np.logical_and(500 <= specWavelength, specWavelength <= 600))]
+    specCropped = spectrum[np.where(np.logical_and(500 <= specWavelength, specWavelength <= 600))]
+    return specCropped,wavelengthCropped
+
+def normalizeSpec():
+    dRef, dRefWavelength = loadDarkRef()
+    spectrum, specWavelength = loadSpectrum()
+    dRefTile = np.tile(dRef, (spectrum.shape[1], 1))
+    return spectrum-dRefTile.T , specWavelength
 
 
 
+
+
+components=loadComponentesSpectra()
+wRef, wRefWavelength=loadWhiteRef()
+
+
+print(dRefTile)
+print()
+
+print(specWavelength)
 
