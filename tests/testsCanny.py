0d06456d875bf074893131a94bbac6f167fa98e8
Emile Marcotte <emilemarcotte@outlook.com>
2021-06-01 11:22:06 -0400

    Did more tests for canny and added more test images

--- tests/testsCanny.py
+++ tests/testsCanny.py
@@ -2,7 +2,8 @@ import envtest
 from skimage.io import imread
 from skimage.feature import canny
 from skimage.color import rgb2gray
-from processImages import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 class TestCanny(envtest.ZiliaTestCase):
 
@@ -10,17 +11,70 @@ class TestCanny(envtest.ZiliaTestCase):
         self.assertTrue(True)
 
     def testImportTestImage(self):
-        image = imread(self.testCannyDirectory+"/cannyTest.jpg")
+        image = imread(self.testCannyDirectory+"/kenyaMedium.jpg")
         self.assertIsNotNone(image)
 
-    def testRGB2Gray(self):
-        image = imread(self.testCannyDirectory+"/cannyTest.jpg")
+    def testRGB2GrayCreatesA2DImage(self):
+        image = imread(self.testCannyDirectory+"/kenyaMedium.jpg")
         self.assertTrue(image.shape[2] == 3)
         grayImage = rgb2gray(image)
         self.assertIsNotNone(grayImage)
         self.assertTrue(len(grayImage.shape) == 2)
         # processedImage = canny(image)
         # self.assertIsNotNone(processedImage)
+
+    def testCannyNeedsA2DImage(self):
+        image = imread(self.testCannyDirectory+"/kenyaMedium.jpg")
+        grayImage = rgb2gray(image)
+        processedImage = canny(grayImage)
+        self.assertIsNotNone(processedImage)
+
+    def testCannySigmaMediumLightKenyaMediumIsEnough(self):
+        image = imread(self.testCannyDirectory+"/kenyaMedium.jpg")
+        grayImage = rgb2gray(image)
+        processedImage = canny(grayImage, sigma=3)
+        # plt.imshow(processedImage)
+        # plt.show()
+
+    def testCannySigmaLowLightRwandaLowIsNotEnough(self):
+        image = imread(self.testCannyDirectory+"/rwandaLow.jpg")
+        grayImage = rgb2gray(image)
+        processedImage = canny(grayImage, sigma=0.5)
+        # plt.imshow(processedImage)
+        # plt.show()
+
+    def testCannySigmaMediumLightBresilMediumIsNotEnough(self):
+        image = imread(self.testCannyDirectory+"/bresilMedium.jpg")
+        grayImage = rgb2gray(image)
+        processedImage = canny(grayImage, sigma=1)
+        # plt.imshow(processedImage)
+        # plt.show()
+
+    def testCannySigmaHighLightSomalieHigh(self):
+        image = imread(self.testCannyDirectory+"/somalieHigh.jpg")
+        grayImage = rgb2gray(image)
+        processedImage = canny(grayImage, sigma=1)
+        # plt.imshow(processedImage)
+        # plt.show()
+
+    def testCannyThresholdSetup(self):
+        image = imread(self.testCannyDirectory+"/kenyaMedium.jpg")
+        grayImage = rgb2gray(image)
+        maximum = np.max(grayImage)
+        self.assertIsNotNone(maximum)
+        multiplicator = maximum/100
+        self.assertIsNotNone(multiplicator)
+
+    def testCannyThresholdLowLightRwandaLow(self):
+        image = imread(self.testCannyDirectory+"/rwandaLow.jpg")
+        grayImage = rgb2gray(image)
+        maximum = np.max(grayImage)
+        multiplicator = maximum/100
+        processedImage = canny(grayImage, sigma=3)
+        # processedImage = canny(grayImage, sigma=3, low_threshold=0.1)
+        # plt.imshow(processedImage)
+        # plt.show()
+
 
 
 if __name__ == "__main__":
