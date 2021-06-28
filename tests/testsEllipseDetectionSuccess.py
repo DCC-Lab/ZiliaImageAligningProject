72dd656941f7493845c30aafb3393ff8754d86c8
Emile Marcotte <emilemarcotte@outlook.com>
2021-06-28 11:08:09 -0400

    more tests on binarizing image

--- tests/testsEllipseDetectionSuccess.py
+++ tests/testsEllipseDetectionSuccess.py
@@ -1,8 +1,10 @@
 import envtest
 import os
 import numpy as np
-from processImages import listFileNames, getFiles
 from skimage.io import imread
+import matplotlib.pyplot as plt
+from processImages import listFileNames, getFiles
+from analyzeRetinaImages import ZilaONHDetector
 
 """
 WARNING: These tests will fail on another computer if the test files path
@@ -36,10 +38,48 @@ class testsEllipseDetectionSuccess(envtest.ZiliaTestCase):
         for i in range(len(sortedInputs)):
             self.assertTrue(sortedInputs[i] == sortedOutputs[i])
 
+    @envtest.skip("skip plots")
     def testOutputImageIntoBinary(self):
-        sortedOutputs = getFiles(inputsPath, newImages=False)
+        sortedOutputs = getFiles(outputsPath, newImages=False)
         testImage = imread(sortedOutputs[0], as_gray=True)
         self.assertTrue(len(testImage.shape) == 2)
+        lower = 0
+        upper = 1
+        threshold = 0.5
+        binaryImage = np.where(testImage >= threshold, upper, lower)
+        plt.figure()
+        plt.subplot(1,2,1)
+        plt.imshow(testImage)
+        plt.subplot(1,2,2)
+        plt.imshow(binaryImage)
+        plt.show()
+
+    def binarizeImage(self, colorImagePath):
+        grayImage = imread(colorImagePath, as_gray=True)
+        lower = 0
+        upper = 1
+        threshold = 0.5
+        binaryImage = np.where(grayImage >= threshold, upper, lower)
+        return binaryImage
+
+    def testBinarizeImage(self):
+        sortedInputs = getFiles(inputsPath, newImages=False)
+        sortedOutputs = getFiles(outputsPath, newImages=False)
+        testInput = imread(sortedInputs[0])
+        testOutput = self.binarizeImage(sortedOutputs[0])
+        plt.figure()
+        plt.subplot(1,2,1)
+        plt.imshow(testInput)
+        plt.subplot(1,2,2)
+        plt.imshow(testOutput, cmap="gray")
+        plt.show()
+
+    def testFindSuccessRateOn1File(self):
+        sortedInputs = getFiles(inputsPath, newImages=False)
+        sortedOutputs = getFiles(outputsPath, newImages=False)
+        testInput = imread(sortedInputs[0])
+        testOutput = self.binarizeImage(sortedOutputs[0])
+
 
 if __name__ == "__main__":
     envtest.main()
