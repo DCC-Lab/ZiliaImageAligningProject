248f41aad32a1f5d2890ebae449fa2b88ab2f20b
emilemarcotte <83650975+emilemarcotte@users.noreply.github.com>
2021-06-30 12:12:43 -0400

    removed old useless comments in main.py

--- main.py
+++ main.py
@@ -64,36 +64,3 @@ Label, dataDictionary = placeRosa(gridParameters, shiftParameters, dataDictionar
 # print(Label)
 
 plotResult(image, shiftParameters, gridParameters)
-
-"""
-for j in range(image.shape[0]):
-    window_name = 'Image'
-    # center_coordinates = (1000-int(indexShift[j,1]),1000-int(indexShift[j,0]))
-    center_coordinates = (int(xRosa[j]), int(yRosa[j]))
-    radius = 30
-    # Blue color in BGR
-    color = (0, 255, 0)
-    # Line thickness of 2 px
-    thickness = 5
-    # Using cv2.circle() method
-    # Draw a circle with blue line borders of thickness of 2 px
-    imageWithCircle = cv2.circle(image[0,:,:], center_coordinates, radius, color, thickness)
-    #pyplot.imsave(str(3*j)+'.jpg', imageWithCircle)
-
-    window_name = 'Image'
-    center_coordinates = (int(xLaser[j]),int(yLaser[j]))
-    radius = int(rLaser[j])
-    # Blue color in BGR
-    color = (0, 0, 255)
-    # Line thickness of 2 px
-    thickness = 5
-    # Using cv2.circle() method
-    # Draw a circle with blue line borders of thickness of 2 px
-    imageWithCircle = cv2.circle(image[j,:,:], center_coordinates, radius, color, thickness)
-    #pyplot.imsave(str(3*j+1)+'.jpg', imageWithCircle)
-
-    imageWithCircle = cv2.circle(laser[j,:,:], center_coordinates, radius, color, thickness)
-    #plt.imshow(image)
-    #plt.show()
-    #pyplot.imsave(str(3*j+2)+'.jpg', imageWithCircle)
-"""
