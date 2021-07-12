601dbd0646cd278f66932112be88780eae36aa90
Daniel <dccote@cervo.ulaval.ca>
2021-07-12 17:58:59 -0400

    Reorganized files

--- database-scripts/ziliaBloodSamplesTables.sql
+++ database-scripts/ziliaBloodSamplesTables.sql
@@ -5,7 +5,7 @@ create table IF NOT EXISTS bloodfiles (path text, md5 text);
 .import "/tmp/bloodfiles.txt"
 
 alter table bloodfiles add column saturation real;
-alter table bloodfiles add column intensity real;
+alter table bloodfiles add column intensity int;
 alter table bloodfiles add column led text;
 alter table bloodfiles add column comment text;
 
@@ -24,21 +24,4 @@ CREATE TABLE bloodspectra (wavelength real, intensity real, column text, md5 tex
 create index md5Idx3 on bloodspectra (md5);
 create index wavelengthIdx2 on bloodspectra (wavelength);
 
-
-# Add date 
-update spectralFiles set date= substr(path, instr(path, '2021'),4) || '-' || substr(path, instr(path, '2021')+4,2) || '-' || substr(path, instr(path, '2021')+6,2) || " " || substr(path, instr(path, '2021')+9,2) || ":" || substr(path, instr(path, '2021')+11,2) || ":" || substr(path, instr(path, '2021')+13,2) where path like '%/2021____-______-%';
-
-# Add monkeyId
-update spectralFiles set monkeyId='1511184' where path like '%bresil%';
-update spectralFiles set monkeyId='1512692' where path like '%kenya%';
-update spectralFiles set monkeyId='1508202' where path like '%1508202%';
-update spectralFiles set monkeyId='1508202' where path like '%somalie%';
-
-# Add target
-update spectralFiles set target='onh' where path like '%onh%';
-update spectralFiles set target='mac' where path like '%mac%';
-
-# Add eye
-update spectralFiles set eye='os' where path like '%-os-%';
-update spectralFiles set eye='od' where path like '%-od-%';
 
